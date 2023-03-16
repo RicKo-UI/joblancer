993dc18a338d8452c1cf94a07371b22925bbac64
RicKo-UI <juanenricoc12@gmail.com>
2023-03-17 01:29:31 +0800

    membuat sign up page

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,6 +1,38 @@
 <template>
     <div class="login py-5">
-      <h1>Hello World</h1>
+      <div class="container mt-3">
+        <div class="row">
+            <div class="col-md-6">
+                <img src="../assets/about.png" alt="" class="img-fluid">
+            </div>
+            <div class="col-md-6 align-self-center">
+                <h3>Sign In</h3>
+                <p class="mb-5">Silahkan masuk dengan akun yang sudah didaftarkan ya</p>
+                <form>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Masukan Email Terdaftar">
+                    </div>
+                    <div class="mb-5">
+                        <label for="exampleInputPassword1" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Masukan Password Valid Kamu">
+                    </div>
+                    <div class="mb-3">
+                        <router-link to="/" class="d-grid gap-2">
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </router-link>
+                        
+                    </div>
+                    <div class="mb-3 d-flex align-items-end">
+                        Belum Punya Akun? 
+                        <router-link to="/" class="link-signin ms-2">
+                            Daftar
+                        </router-link>
+                    </div>
+                    </form>
+            </div>
+        </div>
+      </div>
     </div>
 </template>
   
@@ -17,5 +49,7 @@ export default {
 </script>
   
 <style>
-
+.link-signin{
+    text-decoration: none;
+}
 </style>
